0a3804bfb463839375bbce6c09b61c175b7229f6
anjua <anjua.github@gmail.com>
2022-03-16 19:41:11 +0700

    Commit Profile

--- protected/modules/user/models/Profile.php
+++ protected/modules/user/models/Profile.php
@@ -2,5 +2,31 @@
 
 class Profile extends CActiveRecord
 {
+
+    public $regMode = false;
+	
+	private $_model;
+	private $_modelReg;
+	private $_rules = array();
+
+    public static function model($classname = __CLASS__)
+    {
+        return parent::model($classname);
+    }
+
+    public function tableName()
+    {
+        return Yii::app()->getModule('user')->tabelProfile;
+    }
+    
+    public function relations()
+    {
+        $relations = array(
+            'user' => array(self::HAS_ONE, 'User', 'id')
+        );
+
+        return $relations;
+    }
+
     
 }
